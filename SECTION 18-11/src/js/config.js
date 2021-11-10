37cb8e91bcf340db2b9839dcc83dc9bd2b1e9641
PINKALRAVAL <pinkal@ashutec.com>
2021-11-10 11:58:05 +0530

    add forkifyApp

---
+++ SECTION 18-11/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "a466ad3e-7fa3-4497-95a2-eb12a128ca4a";
+export const MODAL_CLOSE_SEC = 2.5;
